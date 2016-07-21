86a19ac5a61b0c44c998bd8c1c6ab8ecae18f5c8
Sergei Kissel <sergei.kissel@gmail.com>
2016-07-21 12:51:36 +0300

    rockpaperscissors static renamed

--- static.js
+++ static.js
@@ -2,8 +2,6 @@ var express = require('express')
 var path = require('path')
 var app = express()
 
-//app.set('port', (process.env.PORT || 5000))
-
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.listen(process.env.PORT || 5000)
