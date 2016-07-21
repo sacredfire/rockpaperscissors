0e01cf5efdbf2adff941e7b5490ec98d46bdd688
Sergei Kissel <sergei.kissel@gmail.com>
2016-07-21 12:33:52 +0300

    rockpaperscissors static 1st try

---
+++ static.js
@@ -0,0 +1,9 @@
+var express = require('express')
+var path = require('path')
+var app = express()
+
+//app.set('port', (process.env.PORT || 5000))
+
+app.use(path.join(__dirname, 'public'))
+
+app.listen(process.env.PORT || 5000)
